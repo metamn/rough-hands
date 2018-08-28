bfefe7eacc9794f3b2e6c9627b9b27ccf28ee273
metamn <bartus.csongor@gmail.com>
2018-08-28 08:44:45 +0300

    Removing unused template parts / HTML code

--- inc/class-roughhands-template.php
+++ inc/class-roughhands-template.php
@@ -12,6 +12,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 
 /**
+ * Remove credait from header
+ *
+ * @return void.
+ */
+function roughhands_remove_storefront_credit() {
+	remove_action( 'storefront_footer', 'storefront_credit', 20 );
+}
+add_action( 'init', 'roughhands_remove_storefront_credit' );
+
+
+/**
+ * Remove everything after the shop loop except navigation
+ *
+ * See `storefront/inc/woocommerce/storefront-woocommerce-template-hooks.php`
+ *
+ * @return void.
+ */
+function roughhands_remove_storefront_after_shop_loop() {
+	remove_action( 'woocommerce_after_shop_loop', 'storefront_sorting_wrapper', 9 );
+	remove_action( 'woocommerce_after_shop_loop', 'woocommerce_catalog_ordering', 10 );
+	remove_action( 'woocommerce_after_shop_loop', 'woocommerce_result_count', 20 );
+	remove_action( 'woocommerce_after_shop_loop', 'storefront_sorting_wrapper_close', 31 );
+}
+add_action( 'init', 'roughhands_remove_storefront_after_shop_loop' );
+
+
+/**
+ * Remove everything before the shop loop
+ *
+ * See `storefront/inc/woocommerce/storefront-woocommerce-template-hooks.php`
+ *
+ * @return void.
+ */
+function roughhands_remove_storefront_before_shop_loop() {
+	remove_action( 'woocommerce_before_shop_loop', 'storefront_sorting_wrapper', 9 );
+	remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 10 );
+	remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
+	remove_action( 'woocommerce_before_shop_loop', 'storefront_woocommerce_pagination', 30 );
+	remove_action( 'woocommerce_before_shop_loop', 'storefront_sorting_wrapper_close', 31 );
+}
+add_action( 'init', 'roughhands_remove_storefront_before_shop_loop' );
+
+/**
  * Remove cart from header
  *
  * @return void.
